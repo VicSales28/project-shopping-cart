5764965f74b816ade87b3859c23377a84b60b4e9
Victoria Sales <contatovictoriasales@gmail.com>
2023-01-16 18:29:58 -0300

    Adiciona listagem de produtos(Requisito03)

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
 import { searchCep } from './helpers/cepFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
 import './style.css';
+import { createProductElement } from './helpers/shopFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+const computerOptions = await fetchProductsList('computador');
+
+const productSection = document.querySelector('.products');
+
+const showListOfProducts = (products) => {
+  products.forEach((product) => {
+    const htmlComponent = createProductElement(product);
+    productSection.appendChild(htmlComponent);
+  });
+};
+
+showListOfProducts(computerOptions);
