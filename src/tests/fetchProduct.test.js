776c0c3f197978f1d9c93deeb8e4bb684e7cbcaa
Victoria Sales <contatovictoriasales@gmail.com>
2023-01-17 13:10:26 -0300

    Implementa testes na função fetchProdut(Requisito 06)

--- src/tests/fetchProduct.test.js
+++ src/tests/fetchProduct.test.js
@@ -4,7 +4,23 @@ import product from './mocks/product';
 
 // implemente seus testes aqui
 describe('Teste a função fetchProduct', () => {
-  it('...', () => {
-    
+  it('fetchProduct é uma função', () => {
+    expect(typeof fetchProduct).toBe('function');
+  });
+
+  it('fetch é chamado ao executar a função com o argumento do produto "MLB1405519561"', async () => {
+    await fetchProduct('MLB1405519561');
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  it('fetch é chamado com o endpoint correto ao executar a função com o argumento do produto "MLB1405519561"', async () => {
+    await fetchProduct('MLB1405519561');
+    const endPoint = 'https://api.mercadolibre.com/items/MLB1405519561';
+    expect(fetch).toHaveBeenCalledWith(endPoint);
+  });
+
+  it('ao chamar a função fetchProduct sem argumento, retorna um erro com a mensagem: ID não informado.', async () => {
+    const errorMessage = 'ID não informado';
+    await expect(fetchProduct()).rejects.toThrow(new Error(errorMessage));
   });
 });
